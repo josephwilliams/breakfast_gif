5fc2bfcc250e72be8441cf04c1fcbb64b26a4353
josephwilliams <josephwilli12@gmail.com>
2018-03-17 22:06:01 -0700

    added moving_breakfast gif load screen UI for home page

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
+import loadingGif from 'assets/images/moving_breakfast.gif';
+
 import './Grid.css';
 
 
+// TODO: implement conditional UI based on isLoading data or not.
 class Grid extends Component {
   render() {
     return (
-      <div>
-        {'Grid'}
+      <div className={'gridWrapper'}>
+        <img
+          src={loadingGif}
+          className={'loadingIcon'}
+          alt={'loading...'}
+        />
+        <div className={'gridContentWrapper'}>
+        </div>
       </div>
     );
   }
