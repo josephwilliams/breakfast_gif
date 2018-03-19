4a062d22103889234c62f7fcf4a43f379aa03be7
josephwilliams <josephwilli12@gmail.com>
2018-03-18 22:21:56 -0700

    Favorites Page renders Grid

--- src/pages/FavoritesPage/index.js
+++ src/pages/FavoritesPage/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Page from 'components/Page';
+import Grid from 'components/Grid';
 
 
 const FavoritesPage = () => (
   <Page>
-    {'Favorites'}
+    <Grid />
   </Page>
 );
 
