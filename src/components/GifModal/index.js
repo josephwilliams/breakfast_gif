ba593ad222a4bee507438d5f1fe682546bf6bd59
josephwilliams <josephwilli12@gmail.com>
2018-03-18 22:16:42 -0700

    added add favorite actions and dispatch action function to GifModal, all based on gif id (to be used in giphy api request)

--- src/components/GifModal/index.js
+++ src/components/GifModal/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import lodashGet from 'lodash/get';
 
 import Modal from 'react-modal';
+
+import { ACTION_ADD_GIF_TO_FAVORITES_REQUESTED } from 'redux/actions/giphy.js';
+
+import makeAction from 'redux/utils.js';
 
 import './GifModal.css';
 
@@ -25,8 +30,10 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
-const GifModal = ({ modalGifId, closeModal, selectedGifData }) => {
+const GifModal = ({ dispatch, modalGifId, closeModal, selectedGifData }) => {
   // NOTE: if there is a selected 'modalGifId', modal is open. This approach could cause some contention, and admittedly is syntantically less clean than an 'isOpen' bool, but overall works a little better, as you only have to consider a single prop.
+
+  const gifUrl = lodashGet(selectedGifData, 'images.original.url');
 
   const gifData = [
     {
@@ -43,7 +50,11 @@ const GifModal = ({ modalGifId, closeModal, selectedGifData }) => {
     },
   ];
 
-  const gifUrl = lodashGet(selectedGifData, 'images.original.url');
+  const addGifToFavorites = () => (
+    dispatch(makeAction(ACTION_ADD_GIF_TO_FAVORITES_REQUESTED, {
+      gifId: modalGifId,
+    }))
+  );
 
   return (
     <Modal
@@ -52,7 +63,7 @@ const GifModal = ({ modalGifId, closeModal, selectedGifData }) => {
       style={customStyles}
     >
       <div className={'modalContentWrapper'}>
-        <img src={gifUrl} alt={selectedGifData.title} className={'modalGif'} />
+        <img src={gifUrl} className={'modalGif'} alt={selectedGifData.title} />
         <div className={'modalInfoWrapper'}>
           {gifData.map((item, index) => (
             <div className={'modalInfoRow'} key={index}>
@@ -64,6 +75,9 @@ const GifModal = ({ modalGifId, closeModal, selectedGifData }) => {
               </div>
             </div>
           ))}
+          <div onClick={addGifToFavorites} className={'modalAddToFavoritesButton'}>
+            {'Add to Favorites'}
+          </div>
         </div>
       </div>
     </Modal>
@@ -71,6 +85,7 @@ const GifModal = ({ modalGifId, closeModal, selectedGifData }) => {
 };
 
 GifModal.propTypes = {
+  dispatch: PropTypes.func.isRequired,
   modalGifId: PropTypes.string,
   closeModal: PropTypes.func,
   selectedGifData: PropTypes.object,
@@ -80,4 +95,6 @@ GifModal.defaultProps = {
   selectedGifData: {},
 };
 
-export default GifModal;
+const GifModalConnected = connect()(GifModal);
+
+export default GifModalConnected;
